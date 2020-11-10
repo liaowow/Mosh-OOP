3a92e87ad69c0980eb9d1bf1ea52278606c9e1ab
Annie Liao <liaowow@gmail.com>
2020-11-10 15:58:38 -0500

    add ES6 this keyword review

--- es6/index.js
+++ es6/index.js
@@ -1,3 +1,17 @@
+/*** ES6 `this` keyword ***/
+const CircleThis = function() {
+  this.draw = function() { console.log(this) }
+}
+
+const c = new Circle()
+// Method Call -- we are calling the method on an object (in this case: `this` will point to c)
+c.draw()
+
+const draw = c.draw
+// Function Call -- we are calling it as a standalone function
+draw()
+
+/*** ES6 Class and Static Method ***/
 // ES6 Class Declaration
 class Circle {
   constructor(radius) {
@@ -16,9 +30,10 @@ class Circle {
   }
 }
 
-const c = Circle.parse('{ "radius": 1 }')
-console.log(c)
+// const c = Circle.parse('{ "radius": 1 }')
+// console.log(c)
 
+/* before ES6... */
 // function Circle(radius) {
 //   this.radius = radius
 //   this.draw = function() {
